ac7308526a778f95591936d2e443b2efbf041e31
lightskinche <74875815+lightskinche@users.noreply.github.com>
2021-10-26 19:54:56 -0400

    Changed general structure and (almost) completed mod
    All we need to do know is figure out why getBlockEntities() isn't getting any block entities; weird bug, have to find a way to fix it.

--- src/main/java/net/lightskinche/signreader/SignReader.java
+++ src/main/java/net/lightskinche/signreader/SignReader.java
@@ -1,66 +1,61 @@
 package src.main.net.lightskinche.signreader;
 
 import java.io.File;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.UUID;
 
 import org.lwjgl.glfw.GLFW;
 
-import net.minecraft.block.Blocks;
+import com.mojang.datafixers.types.templates.List;
+
+import net.minecraft.block.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.tileentity.*;
+import net.minecraft.world.*;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-
+import java.util.*;
 @EventBusSubscriber(bus = Bus.FORGE, modid = SignReader.MOD_ID, value = Dist.CLIENT)
 @Mod(SignReader.MOD_ID)
 public class SignReader
 {
 	public static final String MOD_ID = "signreader";
 	
-	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent
-	public static void onKeyInput(KeyInputEvent event)
+	public static void onWorldTick(TickEvent.WorldTickEvent event)
 	{
 		Minecraft client = Minecraft.getInstance();
-		
-		if (event.getAction() == GLFW.GLFW_PRESS && event.getKey() == GLFW.GLFW_KEY_R && client.player != null)
+		try 
 		{
-			try 
+			File signFile = new File(client.gameDirectory, "signs.txt");
+			FileWriter fileWriter = new FileWriter(signFile);
+			World level = event.world;
+			if (!signFile.exists())
 			{
-				File signFile = new File(client.gameDirectory, "signs.txt");
-				FileWriter fileWriter = new FileWriter(signFile);
-				
-				if (!signFile.exists())
-				{
 					signFile.createNewFile();
-				}
-				
-				World level = client.level;
-				BlockPos playerPos = client.player.blockPosition();
-				BlockPos startPos = new BlockPos(playerPos.getX() - 100, 0, playerPos.getZ() - 100);
-				BlockPos endPos = new BlockPos(playerPos.getX() + 100, 100, playerPos.getZ() + 100);			
-				for (BlockPos pos : BlockPos.betweenClosed(startPos, endPos))
-				{
-					if (client.level.getBlockState(pos).is(Blocks.OAK_SIGN))
-					{
-						fileWriter.write("(" + pos.toShortString() + ")" + System.lineSeparator());
-					}
-				}
-				fileWriter.close();
 			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
+			Chunk current_chunk = level.getChunk(client.player.blockPosition().getX(), client.player.blockPosition().getZ());
+			//TODO: Find out why we don't get any block entities from here
+			Map<BlockPos, TileEntity> entities = current_chunk.getBlockEntities();
+			for(BlockPos pos : current_chunk.getBlockEntitiesPos()) {
+				fileWriter.write(pos.toShortString());
 			}
+			fileWriter.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
